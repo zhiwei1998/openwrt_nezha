8e6f70902a630159079d46fb909eb3f1fc502e12
root <zhiwei1000@qq.com>
2025-12-29 22:27:45 +0800

    添加自定义配置，修复bug

--- luci-app-nezha-agent-v1/luasrc/model/cbi/nezha-agent-v1.lua
+++ luci-app-nezha-agent-v1/luasrc/model/cbi/nezha-agent-v1.lua
@@ -271,8 +271,7 @@ o.default = uci:get("nezha-agent-v1", "config", "config_mode") or "preset"
 o = s:taboption("basic", TextValue, "custom_config", translate("自定义配置编辑"))
 o.template = "cbi/tvalue"
 o.rows = 30
-o.wrap = "off"
-o.width = "auto"
+o.wrap = "soft"
 o.description = translate("编辑自定义配置文件内容")
 o:depends("config_mode", "custom")
 function o.cfgvalue(self, section)
